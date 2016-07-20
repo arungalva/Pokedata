d6dd209d2fd812c49bb54a9af7480e0f25ebb3f2
Arun Galva <arungalva@cmail.carleton.ca>
2016-07-19 22:40:07 -0400

    finished the pokemon info tab

--- platforms/android/assets/www/js/application.js
+++
@@ -1,7 +0,0 @@
-var records;
-
-records = function($scope) {
-  $scope.records = ["Alfreds Futterkiste", "Berglunds snabbköp", "Centro comercial Moctezuma", "Ernst Handel"];
-};
-
-angular.module('index', []).controller('IndexCtrl', records);
