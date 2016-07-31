d2b9cf8332143d20759e2f0410433a0a19f4f1aa
Arun galva <arungalva@hotmail.com>
2016-07-31 19:22:48 -0400

    minified slidingtabs js file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var bower = require('bower');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -12,12 +14,22 @@ var paths = {
 gulp.task('sass', function(done) {
   gulp.src('./scss/app.scss')
     .pipe(sass({
-      // outputStyle: 'compressed'
+      outputStyle: 'compressed'
     }))
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(rename('./www/css/app.min.css'))
     .on('end', done);
+});
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./www/lib/ion-slide-box-tabs/js/slidingTabsDirective.js'),
+        uglify(),
+        gulp.dest('./www/lib/ion-slide-box-tabs/js/slidingTabsDirective.min.js')
+    ],
+    cb
+  );
 });
 
 gulp.task('watch', function() {
